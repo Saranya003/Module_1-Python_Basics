93ea86c54906532e06ec3502968755e7bd17cc44
Salman0097 <91413898+Salman0097@users.noreply.github.com>
2022-08-27 18:35:40 +0530

    aug 27th

---
+++ ReverseAString.py
@@ -0,0 +1,13 @@
+
+'''gvn_String = input("Enter a string : ")
+revrsed_Str = gvn_String[::-1]
+print(revrsed_Str)'''
+
+gvn_String = input("Enter a string : ")
+def revString():
+    print(gvn_String[1:3])
+
+    revrsed_Str = gvn_String[::-1]
+    print(revrsed_Str)
+    revrsed_Str.isalnum()
+revString()
--- inputInSigleLine.py
+++ inputInSigleLine.py
@@ -1,9 +1,14 @@
-x,y,z=input("Enter three numbers : ").split()
+'''x,y,z=input("Enter three numbers : ").split()
 if (x>y and x>z):
     print("The greatest number is : ",x)
 elif (y>x and y>z):
     print("The greatest number is : ",y)
 else:
-    print("The greatest number is : ",z)
+    print("The greatest number is : ",z)'''
+
+
+# Single variable and multiple input
+value=list(map(int,input("Enter the numbers : ").split(' ')))
+print(*value)
 
     
