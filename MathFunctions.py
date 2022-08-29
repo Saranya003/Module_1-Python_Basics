a8babbb045a593946de18bdb81251854f64ab463
Salman0097 <91413898+Salman0097@users.noreply.github.com>
2022-08-29 20:56:38 +0530

    29 Aug

--- MathFunctions.py
+++ MathFunctions.py
@@ -3,7 +3,9 @@ x=100
 
 print(math.sqrt(x))
 print(math.cos(x))
-print(math.ceil())
-print(math.exp())
-print(math.degrees())
-print(math.lcm())
+print(math.ceil(x))
+print(math.exp(x))
+print(math.degrees(x))
+print(math.lcm(x))
+print(math.cbrt(1000))
+math.
--- forLoopEx.py
+++ forLoopEx.py
@@ -3,11 +3,48 @@ Syntax:
 for value in sequence:
     loop body
 '''
+# Looping through string
 name="Salmeera"
 for letters in name:
     print(letters)
 
+# Looping through string
+fruits=["Maambazham", "VaazhaiPazham", "Sappotta", "Kamala Orange","Maathulai"]
+for x in fruits:
+    print(x)
+# break statement
+#fruits=["Maambazham", "VaazhaiPazham", "Sappotta", "Kamala Orange","Maathulai"]
+for y in fruits:
+    print(y)
+    if y=="VaazhaiPazham":
+        break
+# continue statement
+for z in fruits:
+    if z=="Sappotta":
+        print(z)
+        continue
+#range function
 for i in range(4):
-   # print(i)
-    #print("I am Legend")
-    print("I am Legend", end=" ") # to print in the same line
+    print(i)
+    #print('I am Legend')
+for i in range(7,11):
+    print(i)
+for i in range(12,30,4): # third value increments of i
+    print(i)
+
+# else in for loop
+for i in range(4):
+    print(i)
+else:
+    print("Success")
+
+# Nested loop
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+for x in adj:
+  for y in fruits:
+    print(x, y)
+
+# The pass Statement
+for x in [0,1,2]:
+    pass
