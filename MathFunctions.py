126ab32869e2681597de033f9d10fe2a75421d5e
Salman0097 <91413898+Salman0097@users.noreply.github.com>
2022-08-30 14:02:36 +0530

    Aug 30

--- MathFunctions.py
+++ MathFunctions.py
@@ -1,11 +1,32 @@
-import math
-x=100
+print(max(15,5,100,50))
+print(min(15,5,100,50))
+print(abs(-7.25))
+print(pow(2,3))
 
+import math
+
+x=100
 print(math.sqrt(x))
-print(math.cos(x))
-print(math.ceil(x))
-print(math.exp(x))
-print(math.degrees(x))
-print(math.lcm(x))
-print(math.cbrt(1000))
-math.
+y=math.ceil(1.4)
+z=math.floor(1.4)
+print(y)
+print(z)
+
+a=math.pi
+print(a)
+
+
+
+
+
+
+# import math
+# x=100
+#
+# print(math.sqrt(x))
+# print(math.cos(x))
+# print(math.ceil(x))
+# print(math.exp(x))
+# print(math.degrees(x))
+# print(math.lcm(x))
+# print(math.cbrt(1000))
