68824ed7a7b484c4e1c90eee98d8b6777886a320
Salman0097 <91413898+Salman0097@users.noreply.github.com>
2022-09-07 19:07:36 +0530

    Sep 07

--- PrimeNumbersOrNot.py
+++ PrimeNumbersOrNot.py
@@ -1,11 +1,13 @@
-num=int(input("Enter the number : "))
-if (num>1):
-    for i in range (2,num//2+1):
-        if (num%i==0):
-            print("The number is not a prime number")
-            break
-        else:
-            print("The number is a prime number!!")
-            break
-else:
-    print("The number is not a prime number")
+def primeNoOrNot():
+    num=int(input("Enter the number : "))
+    if (num>1):
+        for i in range (2,num//2+1):
+            if (num%i==0):
+                print("The number is not a prime number")
+                break
+            else:
+                print("The number is a prime number!!")
+                break
+    else:
+        print("The number is not a prime number")
+primeNoOrNot()
